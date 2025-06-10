b3ac3ae67e900fc18fb993938e177231d2f441fa
Charlotte Handford <handford.charlotte@yahoo.co.uk>
2025-06-10 09:48:25 +0100

    Added test

--- tests/test_count_words.py
+++ tests/test_count_words.py
@@ -5,6 +5,10 @@ def test_count_words_returns_int():
     result = count_words("string")
     assert type(result) == int
 
+def test_single_word_string():
+    result = count_words("string")
+    assert result == 1
+
 def test_count_words_returns_correct_length_no_punctuation():
     result = count_words("Test string")
     assert result == 2
